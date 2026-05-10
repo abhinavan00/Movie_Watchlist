360b2e9de5eefc3a88850788c4e61a28b8a6ba3a
Abhinav Anand <abhinavanand072@gmail.com>
2026-05-10 08:23:43 +0530

    header design complete

---
+++ home/index.js
@@ -0,0 +1,6 @@
+const searchBtn = document.getElementById('search-btn')
+const searchInput = document.getElementById('search-input')
+
+searchBtn.addEventListener('click', function() {
+    console.log(searchInput.value)
+})
