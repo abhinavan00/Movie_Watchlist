fcf589ce3bc7ab25fe9c29e0da9aab4cf7203302
Abhinav Anand <abhinavanand072@gmail.com>
2026-05-13 11:21:02 +0530

    test complete of adding results to the DOM

--- home/index.js
+++ home/index.js
@@ -1,5 +1,6 @@
 const searchBtn = document.getElementById('search-btn')
 const searchInput = document.getElementById('search-input')
+const movieListContainer = document.getElementById('movie-list-container')
 
 searchBtn.addEventListener('click', async function() {
     const res = await fetch(`http://www.omdbapi.com/?apikey=2825a6ba&s=${searchInput.value}`)
@@ -8,12 +9,37 @@ searchBtn.addEventListener('click', async function() {
     for await(const item of data.Search) {
         const fetchImbd = await fetch(`http://www.omdbapi.com/?apikey=2825a6ba&i=${item.imdbID}`)
         const imdbData = await fetchImbd.json()
-        console.log(imdbData)
+        // console.log(imdbData.Poster)
+        // console.log(imdbData.Title)
+        // console.log(imdbData.imdbRating)
+        // console.log(imdbData.Runtime)
+        // console.log(imdbData.Genre)
+        // console.log(imdbData.Plot)
+
+        movieListContainer.innerHTML += `
+                <div class="movie-card">
+                        <img src="${imdbData.Poster}" alt="" class="movie-poster">
+                        <div class="movie-details-container">
+                            <div class="movie-header">
+                                <p class="title">${imdbData.Title}</p>
+                                <p class="rating">&#11088 ${imdbData.imdbRating}</p>
+                            </div>
+                            <div class="movie-detail">
+                                <p>${imdbData.Runtime}</p>
+                                <p>${imdbData.Genre}</p>
+                                <button class="watchlist-btn">
+                                    <img src="../images/plus-icon.svg" alt="plus icon">
+                                    Watchlist
+                                </button>
+                            </div>
+                            <p class="about-movie">
+                                ${imdbData.Plot}
+                            </p>
+                        </div>
+                    </div>
+                    <hr>
+            `
     }
 
-    
-    
-    // const fetchImdbDetail = await fetch(`http://www.omdbapi.com/?apikey=2825a6ba&i=${data.Search[0].imdbID}`)
-    // const imdbData = await fetchImdbDetail.json()
-    // console.log(imdbData)
+        
 })
