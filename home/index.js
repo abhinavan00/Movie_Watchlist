38e6c350af98d30c6e541caa56d7505e74df49a4
Abhinav Anand <abhinavanand072@gmail.com>
2026-05-13 10:57:21 +0530

    fetch confirm from api

--- home/index.js
+++ home/index.js
@@ -4,7 +4,14 @@ const searchInput = document.getElementById('search-input')
 searchBtn.addEventListener('click', async function() {
     const res = await fetch(`http://www.omdbapi.com/?apikey=2825a6ba&s=${searchInput.value}`)
     const data = await res.json()
-    console.log(data.Search[0].imdbID)
+
+    for await(const item of data.Search) {
+        const fetchImbd = await fetch(`http://www.omdbapi.com/?apikey=2825a6ba&i=${item.imdbID}`)
+        const imdbData = await fetchImbd.json()
+        console.log(imdbData)
+    }
+
+    
     
     // const fetchImdbDetail = await fetch(`http://www.omdbapi.com/?apikey=2825a6ba&i=${data.Search[0].imdbID}`)
     // const imdbData = await fetchImdbDetail.json()
