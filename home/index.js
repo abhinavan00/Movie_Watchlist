f5e3b91e2eea2c33ef8123c7877c7b707ceaf343
Abhinav Anand <abhinavanand072@gmail.com>
2026-05-13 10:28:29 +0530

    movie card design complete

--- home/index.js
+++ home/index.js
@@ -1,6 +1,12 @@
 const searchBtn = document.getElementById('search-btn')
 const searchInput = document.getElementById('search-input')
 
-searchBtn.addEventListener('click', function() {
-    console.log(searchInput.value)
+searchBtn.addEventListener('click', async function() {
+    const res = await fetch(`http://www.omdbapi.com/?apikey=2825a6ba&s=${searchInput.value}`)
+    const data = await res.json()
+    console.log(data.Search[0].imdbID)
+    
+    // const fetchImdbDetail = await fetch(`http://www.omdbapi.com/?apikey=2825a6ba&i=${data.Search[0].imdbID}`)
+    // const imdbData = await fetchImdbDetail.json()
+    // console.log(imdbData)
 })
