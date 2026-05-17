ca6c45eb84019abcadda69a2f541e05749c57592
Abhinav Anand <abhinavanand072@gmail.com>
2026-05-18 05:10:59 +0530

    Handling Error, if Movie not found.

--- home/index.js
+++ home/index.js
@@ -14,59 +14,70 @@ async function fetchMovie() {
     try {
         const res = await fetch(`http://www.omdbapi.com/?apikey=2825a6ba&s=${searchInput.value}`)
         const data = await res.json()
-        try{
-            const htmlData = data.Search.map(async (item) => {
-                    const imdbFetch = await fetch(`http://www.omdbapi.com/?apikey=2825a6ba&i=${item.imdbID}`)
-                    const imdbData = await imdbFetch.json()
 
-                    return `
-                        <div class="movie-card">
-                                <img src="${imdbData.Poster}" alt="${imdbData.Title}" class="movie-poster">
-                                <div class="movie-details-container">
-                                    <div class="movie-header">
-                                        <p class="title">${imdbData.Title}</p>
-                                        <p class="rating">&#11088 ${imdbData.imdbRating}</p>
+        if (!data.Error) {
+            
+            try{
+                const htmlData = data.Search.map(async (item) => {
+                        const imdbFetch = await fetch(`http://www.omdbapi.com/?apikey=2825a6ba&i=${item.imdbID}`)
+                        const imdbData = await imdbFetch.json()
+    
+                        return `
+                            <div class="movie-card">
+                                    <img src="${imdbData.Poster}" alt="${imdbData.Title}" class="movie-poster">
+                                    <div class="movie-details-container">
+                                        <div class="movie-header">
+                                            <p class="title">${imdbData.Title}</p>
+                                            <p class="rating">&#11088 ${imdbData.imdbRating}</p>
+                                        </div>
+                                        <div class="movie-detail">
+                                            <p>${imdbData.Runtime}</p>
+                                            <p>${imdbData.Genre}</p>
+                                            <button 
+                                                class="watchlist-btn" 
+                                                id='watchlist-btn'
+                                                data-poster='${imdbData.Poster}'
+                                                data-title='${imdbData.Title}'
+                                                data-rating='${imdbData.imdbRating}'
+                                                data-runtime='${imdbData.Runtime}'
+                                                data-genre='${imdbData.Genre}'
+                                                data-plot='${imdbData.Plot}'
+                                                data-id ='${imdbData.imdbID}'
+                                            >
+                                                <img src="../images/plus-icon.svg" alt="plus icon">
+                                                Watchlist
+                                            </button>
+                                        </div>
+                                        <p class="about-movie">
+                                            ${imdbData.Plot}
+                                        </p>
                                     </div>
-                                    <div class="movie-detail">
-                                        <p>${imdbData.Runtime}</p>
-                                        <p>${imdbData.Genre}</p>
-                                        <button 
-                                            class="watchlist-btn" 
-                                            id='watchlist-btn'
-                                            data-poster='${imdbData.Poster}'
-                                            data-title='${imdbData.Title}'
-                                            data-rating='${imdbData.imdbRating}'
-                                            data-runtime='${imdbData.Runtime}'
-                                            data-genre='${imdbData.Genre}'
-                                            data-plot='${imdbData.Plot}'
-                                            data-id ='${imdbData.imdbID}'
-                                        >
-                                            <img src="../images/plus-icon.svg" alt="plus icon">
-                                            Watchlist
-                                        </button>
-                                    </div>
-                                    <p class="about-movie">
-                                        ${imdbData.Plot}
-                                    </p>
                                 </div>
-                            </div>
-                            <hr>
-                    `
-                
-                })
-
-                const html = await Promise.all(htmlData)
-                homeInitialState.style.display = 'none'
-                movieListContainer.style.display = 'block'
-                movieListContainer.innerHTML = html
-
-        } catch(err) {
-            console.log('Some Error Found during the fetch!')
+                                <hr>
+                        `
+                    
+                    })
+    
+                    const html = await Promise.all(htmlData)
+                    homeInitialState.style.display = 'none'
+                    movieListContainer.style.display = 'block'
+                    movieListContainer.innerHTML = html
+                    searchInput.value = ''
+    
+            } catch(err) {
+                console.log('Some Error Found during the fetch!')
+            }
+        } else {
+            movieListContainer.style.display = 'none'
+            homeInitialState.style.display = 'flex'
+            throw new Error()
         }
 
+
     } catch(err) {
-        console.log('Sorry! Unable find the movie.')
-        // console.log(err)
+        homeInitialState.innerHTML = `
+            <p>Movie not found!</p>
+        `
     }
 
         
